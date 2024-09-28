60b4ed77a1d75db448b5a2eeb0f830dbeabfbf5d
PhanDangHung <pdhung130@gmail.com>
2024-09-28 17:17:26 +0700

    done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import BaiTapThucHanhLayout from './Layout/BaiTapThucHanhLayout';
+
+function App() {
+  return (
+    <div className="App">
+        <BaiTapThucHanhLayout/>
+    </div>
+  );
+}
+
+export default App;
